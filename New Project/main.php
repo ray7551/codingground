e167ef0173236a01fbb4bd461bfabedb486188c0
ray7551 <ray7551@gmail.com>
2015-08-09 07:24:14 +0800

    Created by Coding Ground Client

---
+++ New Project/main.php
@@ -0,0 +1,16 @@
+<?php
+$fp = fopen('words.txt', 'r');
+$arr = [];
+while($line = fgets($fp)){
+    $line = strtolower($line);
+    $line = preg_replace('#[,.\s\n]+#', ' ', $line);
+    $words = explode(' ', trim($line));
+    $arr = array_merge($arr, $words);
+}
+
+$count = array_count_values($arr);
+arsort($count);
+
+print_r($count);
+
+fclose($fp);
